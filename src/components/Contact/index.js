851e143eee0e8dfe32fb6d68f058c4a190fc7486
Clayton Goff <cltngoff@gmail.com>
2021-06-20 15:59:50 -0700

    email validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
     const [formState, setFormState] = useState({ name: "", email: "", message: "" });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleChange = (e) => {
-        setFormState({...formState, [e.target.name]: e.target.value })
-
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        console.log('errorMessage', errorMessage);
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value });
+        }
+        
     };
     
     function handleSubmit(e) {
