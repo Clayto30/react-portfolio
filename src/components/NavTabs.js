16cff3504f1307424be00e8b9485a8b3a2f8a913
Clayton Goff <cltngoff@gmail.com>
2021-06-20 15:06:05 -0700

    react bootstrap navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 
 function NavTabs(props) {
     const tabs = ['About', 'Project', 'Contact', 'Resume'];
     return (
         <header>
-            <h1>Clayton Goff</h1>
-            <ul classname="nav nav-tabs">
-                {tabs.map(tab => (
-                    <a href={'#' + tab.toLowerCase()}
-                        onClick={() => props.handlePageChange(tab)}
-                        className={
-                            props.currentPage === tab ? 'nav-link active' : 'nav-link'
-                        }
-                    >
-                        {tab}
-                    </a>
-                ))}
-            </ul>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Clayton Goff</Navbar.Brand>
+                <Nav className="mr-auto">
+                    {tabs.map(tab => (
+                        <Nav.Link a href={'#' + tab.toLowerCase()}
+                            onClick={() => props.handlePageChange(tab)}
+                            className={
+                                props.currentPage === tab ? 'nav-link active' : 'nav-link'
+                            }
+                        >
+                            {tab}
+                        </Nav.Link>
+                    ))}
+                </Nav>
+            </Navbar>
         </header>
     );
 }
